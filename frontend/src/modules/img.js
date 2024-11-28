13b66fe7944e444f7ade036cf572fca4b3a002fc
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-11-28 14:27:45 +0100

    refactor: Update API endpoints to use /api prefix

--- frontend/src/modules/img.js
+++ frontend/src/modules/img.js
@@ -16,9 +16,7 @@ const img = () => {
   // Read all documents - GET
   const getAllImg = async () => {
     try {
-      const response = await fetch(
-        "https://slavic-media-2-0.onrender.com/img/"
-      );
+      const response = await fetch("https://api.slavic.media/img/");
       const data = await response.json();
       state.value.img = data;
     } catch (error) {
@@ -31,7 +29,7 @@ const img = () => {
   const getSpecificImg = async (documentID) => {
     try {
       const response = await fetch(
-        `https://slavic-media-2-0.onrender.com/img/${documentID}`
+        `https://api.slavic.media/img/${documentID}`
       );
       if (!response.ok) {
         throw new Error(`Failed to fetch document with ID: ${documentID}`);
