5c363899395f3a1ac301c303220e71904d21a679
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-11-28 14:43:20 +0100

    refactor: Update API endpoints to use http://localhost:4000/ for local backend server

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,13 +24,4 @@ export default defineConfig({
       },
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:4000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
