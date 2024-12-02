07e615156327ec2e8be8a55d12b1da9db7b0b8bd
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-12-02 16:24:58 +0100

    refactor: Remove vite-svg-loader plugin from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { PrimeVueResolver } from "@primevue/auto-import-resolver";
-import svgLoader from "vite-svg-loader";
 
 export default defineConfig({
   plugins: [
@@ -11,7 +10,6 @@ export default defineConfig({
     Components({
       resolvers: [PrimeVueResolver()],
     }),
-    svgLoader(), // Add this line
   ],
   resolve: {
     alias: {
