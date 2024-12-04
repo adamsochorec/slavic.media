d4bff494f237dd31166135c9a44693ed1d3330ba
Adam S <98590550+adamsochorec@users.noreply.github.com>
2024-12-04 20:29:13 +0100

    refactor: Remove assets route and related code

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -30,7 +30,6 @@ const employee_1 = __importDefault(require("./routes/employee"));
 const legal_1 = __importDefault(require("./routes/legal"));
 const review_1 = __importDefault(require("./routes/review"));
 const event_1 = __importDefault(require("./routes/event"));
-const assets_1 = __importDefault(require("./routes/assets"));
 const services_1 = __importDefault(require("./routes/services"));
 require("dotenv-flow").config();
 // Connect to MongoDB
@@ -53,7 +52,6 @@ app.use("/employee", employee_1.default);
 app.use("/legal", legal_1.default);
 app.use("/review", review_1.default);
 app.use("/event", event_1.default);
-app.use("/assets", assets_1.default);
 app.use("/services", services_1.default);
 // Serve static files from the 'dist' directory
 app.use(express_1.default.static(path_1.default.join(__dirname, "dist")));
