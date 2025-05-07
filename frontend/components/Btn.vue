85a92d0bbfe0322849a5234665c08dfcbf58154e
Adam S. <98590550+adamsochorec@users.noreply.github.com>
2025-05-07 16:51:40 +0200

    Depcheck optimisation

--- frontend/components/Btn.vue
+++ frontend/components/Btn.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { defineEmits } from "vue";
-
 interface Btn {
   label: string;
   icon?: string;
--- frontend/components/Footer.vue
+++ frontend/components/Footer.vue
@@ -142,7 +142,7 @@ const currentYear = new Date().getFullYear();
           rel="noopener noreferrer nofollow"
           href="https://datacvr.virk.dk/enhed/virksomhed/44081512?fritekst=slavic%20media&sideIndex=0&size=10"
           ><span class="underline">Registered company in Denmark</span>
-          <Icon name="cif:dk" class="flag" />
+          <Icon name="flag:dk-4x3" class="flag" />
         </a>
       </nav>
     </div>
