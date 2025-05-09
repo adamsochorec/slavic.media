c8a9f9671975f98f169675be988a608c6dbdc38f
Adam Sochorec <98590550+adamsochorec@users.noreply.github.com>
2025-05-09 10:04:51 +0200

    Disable lightbox trigger in Photo.vue

--- frontend/components/Photo.vue
+++ frontend/components/Photo.vue
@@ -9,6 +9,7 @@ interface Photo {
 }
 const props = defineProps<Photo>();
 
+/*
 const emit = defineEmits(["update:originalWidth", "update:originalHeight"]);
 
 const updateDimensions = (event: Event) => {
@@ -16,29 +17,32 @@ const updateDimensions = (event: Event) => {
   emit("update:originalWidth", target.naturalWidth);
   emit("update:originalHeight", target.naturalHeight);
   target.src = target.dataset.src!;
-};
+}; */
 </script>
 
 <template>
-  <a
-    :href="`https://cdn.slavic.media/img/${id}/public`"
-    :title="title"
-    :data-pswp-width="originalWidth"
-    :data-pswp-height="originalHeight"
-    :data-cropped="true"
-    class="lightbox"
-  >
+<!--
+<a
+  :href="`https://cdn.slavic.media/img/${id}/public`"
+  :title="title"
+  :data-pswp-width="originalWidth"
+  :data-pswp-height="originalHeight"
+  :data-cropped="true"
+  class="lightbox"
+>
+</a>
+-->
     <img
-      class="reveal"
       :src="`https://cdn.slavic.media/img/${id}/public`"
       :alt="alt"
     />
-  </a>
+
 </template>
 
 <style scoped lang="scss">
+/*
 img,
 a {
   cursor: pointer;
-}
+}*/
 </style>
