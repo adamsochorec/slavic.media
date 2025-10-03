d9ea1297d262c4345501a084c4504d887282adc1
Adam Sochorec <98590550+adamsochorec@users.noreply.github.com>
2025-10-03 02:10:03 +0200

    Set employee email as explicitly optional

--- backend/src/models/employee.ts
+++ backend/src/models/employee.ts
@@ -20,7 +20,7 @@ const EmployeeSchema = new Schema({
   department: { type: String, required: true, maxlength: 100 },
   description: { type: String, required: true, maxlength: 500 },
   flag: { type: String, required: true, maxlength: 50 },
-  email: { type: String, maxlength: 100 },
+  email: { type: String, required: false, maxlength: 100 },
   birthday: { type: Date, required: true },
   linkedin: { type: String, maxlength: 100 },
   github: { type: String, maxlength: 100 },
