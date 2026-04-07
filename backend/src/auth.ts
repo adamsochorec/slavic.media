eecf5b6370c188a8aca68c7e0f9d32e4347610f7
Adam Sochorec <98590550+adamsochorec@users.noreply.github.com>
2026-04-07 14:25:25 +0200

    Reset changes

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -11,7 +11,7 @@ export async function registerAndLogin(user = testUser) {
   // Register user (ignore errors if already exists)
   await request(app).post("/user/register").send(user);
 
-  // Login and get tokenss
+  // Login and get token
   const res = await request(app).post("/user/login").send({
     email: user.email,
     password: user.password,
